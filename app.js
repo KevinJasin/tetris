e68c5e50bac552b9e470ed3b6aa928cb2739ffc5
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2024-02-15 15:57:01 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+import { GameBoard } from "./src/GameBoard.js";
+import { OBlock } from "./src/OBlock.js";
+
+// const messageDiv = document.getElementById('message');
+// const scoreDiv = document.getElementById('score');
+// const resetBtn = document.getElementById('reset');
+
+let timeoutID, speed;
+
+const boardSizeX = 14;
+const boardSizeY = 16;
+
+function initGame () {
+
+    clearTimeout(timeoutID);
+    timeoutID = null;
+
+    // score = 0;
+    speed = 500;
+
+    // messageDiv.innerText = '';
+    // messageDiv.classList.add('hidden');
+
+
+    const gameBoard = new GameBoard(boardSizeX, boardSizeY);
+    const oBlock = new OBlock(boardSizeX, boardSizeY);
+    gameBoard.addNewBlock(oBlock);
+    gameBoard.draw();
+
+    (function repeat() {
+        timeoutID = setTimeout(repeat, speed);
+        if ( gameBoard.canGoDown(oBlock) ) {
+            oBlock.moveDown();
+        }
+        gameBoard.draw();
+    })();
+
+    // addFood();
+    // updateHighScore();
+    
+}
+
+initGame();
