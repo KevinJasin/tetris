18bb010786777b0c3a094173560e1570e4c1e2c1
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2024-02-15 15:57:17 +0200

    Update GameBoard.js

--- GameBoard.js
+++ GameBoard.js
@@ -1,23 +1,73 @@
-const GameBoard = {
-    gameBoardTable: document .getElementById("gameboard"),
-    boardSizeX: 12,
-    boardSizeY: 18,
-    draw() {
-        this.gameBoardTable.innerHTML = "";
+class GameBoard {
+    
+    state = [];
+    gameBoardTable = document.getElementById('gameboard');
+    boardSizeX;
+    boardSizeY;
+    currentBlock;
 
-        for (let y = 0; y < this.boardSizeY; y++) {
-            const boardRowTr = document.createElement("tr");
-            for (let x = 0; x < this.boardSizeX; x++) {
-                const boardCellTd = document.createElement("td");
-                const id = y + "_" + x;
-                boardCellTd.setAttribute("id", id);
+    constructor ( boardSizeX, boardSizeY ) {
+
+        this.boardSizeX = boardSizeX;
+        this.boardSizeY = boardSizeY;
+
+        for ( let y = 0; y < this.boardSizeY; y++ ) {
+            let arr = [];
+            for ( let x = 0; x < this.boardSizeX; x++ ) {
+                arr.push("");
+            }
+            this.state.push(arr);
+        }
+   
+    
+        document.addEventListener ('keydown', e => {
+            switch ( e.key ) {
+                // case 'ArrowUp':
+                //     direction = 'u';
+                //     break;
+                // case 'ArrowDown':
+                //     direction = 'd';
+                //     break;
+                case 'ArrowLeft':
+                    this.currentBlock.moveLeft();
+                    this.draw();
+                    break;
+                case 'ArrowRight':
+                    this.currentBlock.moveRight();
+                    this.draw();
+                    break;
+            }
+        });
+    }
+
+    draw() {
+
+        this.gameBoardTable.innerHTML = '';
+        
+        for ( let y = 0; y < this.boardSizeY; y++ ) {
+            const boardRowTr = document.createElement('tr');
+            for ( let x = 0; x < this.boardSizeX; x++ ) {
+                const boardCellTd = document.createElement('td');
+                const id = y + '_' + x;
+                boardCellTd.setAttribute('id', id);
+
+                // draw block
+                if ( this.currentBlock.isOverlapingCell(id) ) {
+                    boardCellTd.classList.add(this.currentBlock.class);
+                }
 
                 boardRowTr.append(boardCellTd);
             }
             this.gameBoardTable.append(boardRowTr);
         }
-
+    
+        // scoreDiv.innerText = 'Score: ' + score;
     }
+
+    addNewBlock( block ) {
+        this.currentBlock = block;
+    }
+
 }
 
-export {GameBoard}
+export { GameBoard }
